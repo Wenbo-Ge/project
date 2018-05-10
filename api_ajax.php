ddfabbebc893219665021bd4e4e4bcea61ed196d
Wenbo-Ge <wge19920921@gmail.com>
2018-05-10 00:26:38 +0000

    updates

--- api_ajax.php
+++ api_ajax.php
@@ -12,13 +12,19 @@
 				$stmt = $conn->prepare("SELECT * FROM Players where jerseyNumber=$jerseyNumber"); 
 			    $stmt->execute();
 			    if($stmt->rowCount()==0){
-			    	echo "No player is currently wearing this jersey, try another number!";
+			    	echo json_encode(array('description'=>"No player is currently wearing this jersey, try another number!",
+			    		'image'=>'https://media-public.fcbarcelona.com/20157/28952/947008/1.0/947008.png?t=1525034444000',
+			    		'name'=>'Whoops....'
+			    )) ;
 			    }else{
 					 while($row = $stmt->fetch()){
 				
 				header('Access-Control-Allow-Origin:*');//加*是允许所有人来请求
 				header('Content-Type:application/json; charset=UTF-8');		    	
-				echo json_encode(array('content'=>$row['name']. $row['description'])) ;
+				echo json_encode(array('description'=>$row['description'],
+										'image'=>$row['url'],
+										'name'=>$row['name']
+										)) ;
 											
 				
 												}
